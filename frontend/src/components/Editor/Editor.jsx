1c85c3897c9650d16178d210126a4a775c12f677
Lucky <luckych8080@gmail.com>
2021-12-17 12:16:07 +0530

    fix some bugs like description

--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -3,14 +3,16 @@ import AceEditor from "react-ace";
 
 import Paper from "@mui/material/Paper";
 
+import 'ace-builds';
+import 'ace-builds/webpack-resolver';
+
 import Beautify from "ace-builds/src-noconflict/ext-beautify";
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "./acebuilds";
 
 import langForEditor from "./langForEditor";
 
-import 'ace-builds';
-import 'ace-builds/webpack-resolver';
+
 
 const Editor = (props) => {
   const lang = langForEditor(props.lang);
